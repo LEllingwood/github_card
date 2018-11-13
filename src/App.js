a1d9f81c725f1a9672116ac368af6855239c7a38
Lea Ellingwood <leaellingwood@hotmail.com>
2018-11-12 22:06:13 -0500

    step 2 completed, working on adding conditional rendering. downloaded material ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+let fetchLink = "https://api.github.com/users/LEllingwood"
 
 class App extends Component {
 
-  handleClick = () => {
-    console.log("this is;", this)
+  state = {
+    user: {},
+    active: false,
   }
 
+  handleClick = () => {
+    console.log("this is;", this)
+    fetch(fetchLink)
+      .then(response => response.json())
+      .then(data => this.setState({user: data.login, active: true}))
+  }
+
+  
+
   render() {
+
+    // const isLoggedIn = this.state.active;
+    // let button;
+
+    // if(isLoggedIn){
+      // return <greetingInfo />;
+        //}
+      // return <loginPrompt />
+        }
+    //  
+    
+    // separate component called greetingInfo that shows: 
+    // data.login
+    //   data.avatar_url
+    //   data.bio
+    //   data.followers_url
+    // }
+
+    // separate component called loginPrompt that shows: "Click to log in"
+
     return (
       <div className="App">
         <header className="App-header">
-          <button onClick={this.handleClick}>Click Me</button>
+          <button onClick={this.handleClick}>Log In</button>
         </header>
       </div>
     );
